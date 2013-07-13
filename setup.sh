ca6ffa9abc400b74b1f593ee79f56cd693491cb1
Ubuntu <ubuntu@ip-172-31-35-5.us-west-2.compute.internal>
2013-07-13 19:05:26 +0000

    install default requirements for Python / Django app  on Ubuntu 12.04 LTS

---
+++ setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Setup.sh for configuring Ubuntu 12.04 LTS EC2 Instance
+
+#update Linux packages
+
+apt-get update
+apt-get upgrade
+apt-get dist-upgrade
+
+# install environnement:
+apt-get install python-pip git mysql-client mysql-server python-mysqldb nginx
+
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+sudo apt-get install -y git-core
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# Load nvm and install latest production node
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.12
+nvm use v0.10.12
+
+# Install jshint to allow checking of JS code within emacs
+# http://jshint.com/
+npm install -g jshint
+
+# Install rlwrap to provide libreadline features with node
+# See: http://nodejs.org/api/repl.html#repl_repl
+sudo apt-get install -y rlwrap
+
+# Install emacs24
+
+sudo apt-add-repository -y ppa:cassou/emacs
+sudo apt-get update
+sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+git clone https://github.com/startup-class/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
+
+
+#config git global user.name
+git config --global "gilles.langevin"
+git config --global "langevin.net@gmail.com"
+
+# Add git setup and close
+git remote add origin https://github.com/guilsdenice/setup.git
+git clone https://github.com/guilsdenice/setup.git
+
